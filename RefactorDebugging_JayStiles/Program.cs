692a5d9a1ec9622014df5c9dcffd6847959bf7bb
Jay Stiles <JayStiles@quickenloans.com>
2018-11-02 15:52:07 -0400

    commiting

---
+++ RefactorDebugging_JayStiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorDebugging_JayStiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6 };
+            int i = 0;
+            for (i = 0; i < array1.Length; i++)
+            {
+                array1[i]++;
+            }
+            int result = GetSum(array1);
+            Console.WriteLine(result);
+        }
+        private static int GetSum(int[] array1)
+        {
+            int sum = 1;
+            foreach (int item in array1)
+            {
+                sum = sum + item;
+            }
+            return sum;
+        }
+    }
+   }
+
